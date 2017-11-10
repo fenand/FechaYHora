e9f4434ac58eea50300e4b09be67e3ffa56a0869
Fernando Garcia De Castro <garciaf4c@gmail.com>
2017-11-10 13:31:23 +0100

    Commit 1

---
+++ FechaYHora/FechaYHora.java
@@ -0,0 +1,18 @@
+public class FechaYHora
+{
+    private NumberDisplay hours;
+    private NumberDisplay minutes; 
+    private  CalendarioBasico dia;
+    private  CalendarioBasico mes;
+    private  CalendarioBasico año;
+
+    public FechaYHora ()
+    {
+       hours = new NumberDisplay(00);
+       minutes = new NumberDisplay(00);
+        
+       
+       
+    }
+
+}
